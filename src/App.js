245c46a366a74c49f9b403367eff2de3278ca709
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-05-05 14:50:19 -0700

    set list state, default todos, add items function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Contact from './Contact'
 import Nav from './components/Nav'
-import Todos from './components/Todos'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import TodoForm from './components/TodoForm';
 
+
+const Todo = ({ todo }) => <div className="todo">{todo.text}</div>;
 
 function App() {
-  
+  const [todoItem, setTodoItem] = useState([
+    {
+      text: "Struggle with React"
+    },
+    {
+      text: "Flail around blindly on the computer"
+    },
+    {
+      text: "Become master programmer"
+    },
+  ])
+
+  const addTodo = text => {
+    const newTodos = [...todoItem, {text}]
+    setTodoItem(newTodos);
+  };
+
   return (
     <Router>
       <div className="App">
-        <Nav />
-        <Switch>
-          <Route path="/todos" exact component={Todos} />
-          <Route path="/contact" component={Contact} />
-        </Switch>
+        <div className="todo-list">
+          <Nav />
+          <Switch>
+            <Route path="/" exact component={TodoForm} />
+            <Route path="/contact" component={Contact} />
+          </Switch>
+          <TodoForm addTodo={addTodo} />
+          {todoItem.map((todo, index) => (
+            <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            />
+          ))}
+        </div>
       </div>
     </Router>
     
