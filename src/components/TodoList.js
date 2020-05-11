544269eeb41880ce56f7bdf5574802ad7bc65a5a
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-05-10 22:55:35 -0700

    error handling, put todo render into separate func

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,41 +4,52 @@ import { v4 as uuidv4 } from 'uuid';
 function TodoList(){
   const [newInput, setNewInput] = useState('')
   const [todoItems, setTodoItems] = useState([])
+  const [error, setError] = useState('');
 
   //Stores the state of input field
-  function handleInputChange(e){
+  const handleInputChange = (e) => {
     e.preventDefault();
     setNewInput(e.target.value)
   }
 
   //Stores the state of submitted input in a list
-  function handleTodoItems(e){
+  const handleTodoItems = (e) => {
     e.preventDefault();
     if (newInput==="") {
-      return (alert("Input field cannot be empty"))
-    }
-    setTodoItems([...todoItems, {id: uuidv4(), text: newInput}]);
-    e.target.reset();
-    setNewInput("");
+      setError("Input field cannot be empty");
+     //goes through each element of todoItems list and finds the element that's the same as newInput
+    } else if ( (typeof todoItems.find( ({text}) => text === newInput)) !== 'undefined'  ) {
+      setError('Todo List item cannot be duplicate')
+    } else {
+      setTodoItems([...todoItems, {id: uuidv4(), text: newInput}]);
+      setError('')
+      e.target.reset();
+      setNewInput("");
+      }
   }
 
-  function deleteTodo(id) {
+  const deleteTodo = (id) => {
+    //goes through the todo list, and returns the todos that aren't equal
+    //to the id that was clicked on
     setTodoItems(todoItems.filter((todoItem) => todoItem.id !== id))
   }
+
+  const generateTodo = todoItems.map((todoItem) => (
+    <li className="todo col-md-6 offset-md-3" key={todoItem.id}>
+      {todoItem.text}
+    <button className="btn btn-danger" onClick={() => deleteTodo(todoItem.id)}>X</button>
+    </li>
+    ));
   
   return (
-      <div className="App">
+      <div className="App text-center">
         <h1>Manage Your Todo List</h1>
-        <form className="todo-list" onSubmit={handleTodoItems}>
-          <input className="todo-form" placeholder="Enter your todo here" onChange={handleInputChange} />
+        <form className="todo-list text-center" onSubmit={handleTodoItems}>
+          <input size="30"className="todo-form" placeholder="Enter your todo here" value={newInput}onChange={handleInputChange} />
           <button className="todo-form btn btn-success" type="submit">Add</button>
-          <ul>
-            {todoItems.map((todoItem) => (
-            <li className="todo col-4 offset-4" key={todoItem.id}>{todoItem.text}
-            <button className="btn btn-danger" onClick={() => deleteTodo(todoItem.id)}>Delete</button>
-            </li>
-              
-            ))}
+          <div className="error-message">{error}</div>
+          <ul className="remove-default-padding">
+            {generateTodo}
           </ul>
         </form>
       </div>
