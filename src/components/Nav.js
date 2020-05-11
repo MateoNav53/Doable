d99b23fda9d09637526abeb166898265370673e5
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-05-10 22:52:55 -0700

    fixed router & added hamburger menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,15 +4,24 @@ import {Link} from 'react-router-dom';
 
 function Nav() {
   return (
-      <nav className="navbar">
-          <h2>Doable - React Todo App</h2>
-          <ul className="nav-links">
-              {/* <Link to='/'> */}
-                  <li>Todos</li>
-              {/* </Link> */}
-              {/* <Link to='/contact'> */}
-                  <li>Contact</li>
-              {/* </Link> */}
+      <nav className="navbar navbar-expand-sm navbar-light">
+          <h2>DoAble - Todo App</h2>
+          <ul className="nav-links navbar-nav navbar-brand">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#link_targets">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+              <div className="collapse navbar-collapse" id="link_targets">
+                <Link to='/'>
+                    <li className="nav-item">
+                        <a className="nav-link"href="#">Todos</a>
+                    </li>
+                </Link>
+                <Link to='/contact'>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Contact</a>
+                    </li>
+                </Link>
+              </div>
           </ul>
       </nav>
   );
