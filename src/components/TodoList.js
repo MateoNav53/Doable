58ff771c28cb7e7959b2d85b4b6e8a36619fc3a6
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-05-14 16:06:44 -0700

    added state to disable edit button while editing

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,6 +5,7 @@ function TodoList(){
   const [newInput, setNewInput] = useState('')
   const [todoItems, setTodoItems] = useState([])
   const [error, setError] = useState('');
+  const [editState, setEditState] = useState(false);
 
   //Stores the state of input field
   const handleInputChange = (e) => {
@@ -14,6 +15,10 @@ function TodoList(){
 
   //Stores the state of submitted input in a list
   const handleTodoItems = (e) => {
+    //If we are submitting an edit, we want to exit the edit state so buttons are available
+    if (editState) {
+      setEditState(false)
+    }
     e.preventDefault();
     if (newInput==="") {
       setError("Input field cannot be empty");
@@ -29,8 +34,6 @@ function TodoList(){
   }
 
   const deleteTodo = (id, e) => {
-    //goes through the todo list, and returns the todos that aren't equal
-    //to the id that was clicked on
     e.preventDefault()
     setTodoItems(todoItems.filter((todoItem) => todoItem.id !== id))
   }
@@ -40,15 +43,16 @@ function TodoList(){
   //delete the todo with the id that was clicked on
   //set the state to be the text of the object from the variable. that returns it to the input field. 
   const editTodo = (id, e) => { 
-    e.preventDefault()
-    const todoObject = todoItems.find((todoItem) => todoItem.id === id)
-    deleteTodo(id, e)
-
-    setNewInput(todoObject.text)
-    //if we click edit on two separate items, it deletes the first one we clicked
-    //need to find out how to maybe disable the all edit buttons to prevent this
-    //or 
-
+    //if we aren't in the edit state, run the normal edit function.
+    //We are in the edit state when the edit button is pressed
+    if(!editState){
+      e.preventDefault()
+      const todoObject = todoItems.find((todoItem) => todoItem.id === id)
+      deleteTodo(id, e)
+      setNewInput(todoObject.text)
+      //if we run the edit function, edit disabling state is set to true. 
+      setEditState(true)
+    }
   }
 
 
@@ -56,7 +60,7 @@ function TodoList(){
     <li className="todo col-md-6 offset-md-3" key={todoItem.id} >
       {todoItem.text}
     <div>
-      <button className="btn btn-info" onClick={(e) => editTodo(todoItem.id, e)}>Edit</button>
+      <button disabled={editState} id="edit-btn"className="btn btn-info" onClick={(e) => editTodo(todoItem.id, e)}>Edit</button>
       <button className="btn btn-danger" onClick={(e) => deleteTodo(todoItem.id, e)}>Delete</button>
     </div>
     </li>
