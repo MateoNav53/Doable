d53c6ca05a2003f9c7a2535251430219f8c15224
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-05-14 20:38:34 -0700

    Updates

---
+++ precache-manifest.30cd4016af967365f4620cf9fa304b60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c0d59a27064cd68738b9a26b157d3a2",
+    "url": "/mateonav53.github.io/doable/index.html"
+  },
+  {
+    "revision": "7dd26453408ceee52e84",
+    "url": "/mateonav53.github.io/doable/static/css/main.f7aa070b.chunk.css"
+  },
+  {
+    "revision": "79f7d47f6fb66bc04158",
+    "url": "/mateonav53.github.io/doable/static/js/2.12ab526c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mateonav53.github.io/doable/static/js/2.12ab526c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dd26453408ceee52e84",
+    "url": "/mateonav53.github.io/doable/static/js/main.5b285e46.chunk.js"
+  },
+  {
+    "revision": "79a06b1391640c6582de",
+    "url": "/mateonav53.github.io/doable/static/js/runtime-main.e6bf6034.js"
+  }
+]);
