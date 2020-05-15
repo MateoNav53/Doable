6fbb880324bbe14317d94d514306dec4a73fa221
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-05-14 22:32:48 -0700

    Updates

--- precache-manifest.1c0d97df5028a00aca7f28584db9b406.js
+++ precache-manifest.1c0d97df5028a00aca7f28584db9b406.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d2bb0c079a61a1d3b67a6d831731c08c",
+    "revision": "04e466e0821854d387c5b1af21c0f936",
     "url": "/doable/index.html"
   },
   {
-    "revision": "d9aa6713402b86c78efd",
+    "revision": "655d3552a26a7f75ef75",
     "url": "/doable/static/css/main.f7aa070b.chunk.css"
   },
   {
-    "revision": "7894c87ab0d325f7e5e9",
-    "url": "/doable/static/js/2.bb9b6a49.chunk.js"
+    "revision": "79f7d47f6fb66bc04158",
+    "url": "/doable/static/js/2.12ab526c.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/doable/static/js/2.bb9b6a49.chunk.js.LICENSE.txt"
+    "url": "/doable/static/js/2.12ab526c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d9aa6713402b86c78efd",
-    "url": "/doable/static/js/main.98dd9994.chunk.js"
+    "revision": "655d3552a26a7f75ef75",
+    "url": "/doable/static/js/main.d65e12ad.chunk.js"
   },
   {
     "revision": "b0f0a49539764ca3413c",
