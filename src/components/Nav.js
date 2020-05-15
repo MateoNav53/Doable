15f410616f03b7aa76ace00eee1b6fd77f5c696f
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-05-14 18:09:26 -0700

    added extra class to disable cursor on site name

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom';
 function Nav() {
   return (
       <nav className="navbar navbar-expand-sm navbar-light">
-          <h2>DoAble - Todo App</h2>
+          <h2 class="doable-header">DoAble - Todo App</h2>
           <ul className="nav-links navbar-nav navbar-brand">
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#link_targets">
             <span className="navbar-toggler-icon"></span>
